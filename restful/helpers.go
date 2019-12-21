7d99fef3ea53c59a0377aed75b484cdd5434b74d
Mohsen Samiei <eng.msamiee@gmail.com>
2019-12-21 15:35:53 +0330

    refactor: restful

--- restful/helpers.go
+++ restful/helpers.go
@@ -3,7 +3,7 @@ package restful
 import (
 	"encoding/json"
 	"encoding/xml"
-	"errors"
+	"github.com/mohsensamiei/golaunch/errorext"
 	"net/http"
 	"strings"
 )
@@ -17,16 +17,32 @@ const (
 	JSONContentType = "application/json"
 )
 
-var (
-	errorInvalidContentType   = errors.New("Invalid request content type")
-	errorInvalidAuthorization = errors.New("Invalid bearer authorization header")
-)
-
-// SendResponse by model and status code
-func SendResponse(response http.ResponseWriter, request *http.Request, statusCode int, model interface{}) {
+// SendStandard by model
+func SendStandard(response http.ResponseWriter, request *http.Request, model interface{}) {
+	var status int
 	switch value := model.(type) {
+	case nil:
+		status = http.StatusNoContent
+	case errorext.IHandledError:
+		status = value.StatusCode()
 	case error:
-		model = NewErrorDTO(value)
+		status = http.StatusInternalServerError
+	default:
+		if request.Method == http.MethodPost {
+			status = http.StatusCreated
+		} else {
+			status = http.StatusOK
+		}
+	}
+	Send(response, request, status, model)
+}
+
+// Send by model and status code
+func Send(response http.ResponseWriter, request *http.Request, statusCode int, model interface{}) {
+	switch value := model.(type) {
+	case errorext.IHandledError:
+	case error:
+		model = errorext.NewInternal(value)
 	}
 
 	var bytes []byte
@@ -49,39 +65,44 @@ func SendResponse(response http.ResponseWriter, request *http.Request, statusCod
 	}
 
 	response.WriteHeader(statusCode)
-	response.Write(bytes)
+	_, _ = response.Write(bytes)
 }
 
-// AddResponseHeaders add header list to response
-func AddResponseHeaders(response http.ResponseWriter, headers http.Header) {
+// AddHeaders add header list to response
+func AddHeaders(response http.ResponseWriter, headers http.Header) {
 	for key, value := range headers {
 		response.Header()[key] = value
 	}
 }
 
-// ParseRequestBody parsing request body to model
-func ParseRequestBody(request *http.Request, model interface{}) error {
+// ParseBody parsing request body to model
+func ParseBody(request *http.Request, model interface{}) error {
 	switch request.Header.Get(ContentType) {
 	case XMLContentType:
-		return xml.NewDecoder(request.Body).Decode(&model)
+		if err := xml.NewDecoder(request.Body).Decode(&model); err != nil {
+			return errorext.NewValidationError("invalid request xml body", err)
+		}
 	case JSONContentType:
-		return json.NewDecoder(request.Body).Decode(&model)
+		if err := json.NewDecoder(request.Body).Decode(&model); err != nil {
+			return errorext.NewValidationError("invalid request json body", err)
+		}
 	default:
-		return errorInvalidContentType
+		return errorext.NewValidationError("invalid request content type")
 	}
+	return nil
 }
 
-// GetAuthBearerHeader get Bearer from header
-func GetAuthBearerHeader(request *http.Request) (string, error) {
+// GetBearerToken get bearer token from header
+func GetBearerToken(request *http.Request) (string, error) {
 	authorization := request.Header.Get("Authorization")
 	dump := strings.Split(authorization, "Bearer")
 	if len(dump) != 2 {
-		return "", errorInvalidAuthorization
+		return "", errorext.NewAuthorizeError("invalid authorization bearer token")
 	}
 	return strings.TrimSpace(dump[1]), nil
 }
 
-// IsSuccessStatusCode return true if http status code is in success
-func IsSuccessStatusCode(statusCode int) bool {
+// IsSuccess return true if http status code is in success
+func IsSuccess(statusCode int) bool {
 	return statusCode >= 200 && statusCode <= 299
 }
